99392ee253ffb8f555424e34d4a8066d6658b674
이용인 <yongin0517@gmail.com>
2024-11-09 16:49:56 +0900

    feat: controller 및 실행 함수 작성

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,5 +1,28 @@
 package store
 
+import store.domain.Controller
+import store.domain.StoreManager
+import store.domain.product.ProductParser
+import store.domain.promotion.PromotionParser
+import store.domain.promotion.PromotionProcessor
+import store.util.InputValidator
+import store.view.InputView
+import store.view.OutputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    val outputView = OutputView()
+    val promotionParser = PromotionParser()
+    val productParser = ProductParser(promotionParser)
+    val storeManager = StoreManager(productParser, promotionParser)
+    val inputView = InputView(outputView)
+    val promotionProcessor = PromotionProcessor(inputView)
+    val inputValidator = InputValidator()
+    val controller = Controller(
+        inputView = inputView,
+        inputValidator = inputValidator,
+        storeManager = storeManager,
+        outputView = outputView,
+        promotionProcessor = promotionProcessor
+    )
+    controller.run()
 }
