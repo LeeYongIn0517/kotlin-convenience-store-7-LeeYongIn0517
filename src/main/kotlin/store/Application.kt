9a07bc05ccbb892ace95f9713a5ac23c63729f5f
이용인 <yongin0517@gmail.com>
2024-11-10 17:33:37 +0900

    fix: 객체 변경 반영

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,40 +1,59 @@
 package store
 
-import store.domain.Controller
 import store.domain.ReceiptManager
 import store.domain.StoreManager
-import store.domain.input.InputController
-import store.domain.input.InputValidator
+import store.domain.controller.Controller
+import store.domain.controller.InputController
+import store.domain.controller.PromotionController
+import store.domain.controller.ReceiptController
 import store.domain.product.ProductParser
-import store.domain.promotion.*
+import store.domain.promotion.FreeItemManager
+import store.domain.promotion.PromotionCalculator
+import store.domain.promotion.PromotionHandler
+import store.domain.promotion.PromotionParser
+import store.domain.validator.InputValidator
+import store.domain.validator.PromotionValidator
 import store.view.InputView
 import store.view.OutputView
 
 fun main() {
+    // Input 관련 객체 생성
     val inputValidator = InputValidator()
+    val inputView = InputView()
+    val inputController = InputController(inputView, inputValidator)
+
+    // Output 관련 객체 생성
     val outputView = OutputView()
-    val inputView = InputView(outputView)
+
+    // Promotion 관련 객체 생성
     val promotionParser = PromotionParser()
-    val productParser = ProductParser(promotionParser)
-    val storeManager = StoreManager(productParser, promotionParser)
-    val freeItemManager = FreeItemManager()
     val promotionCalculator = PromotionCalculator()
-    val promotionHandler = PromotionHandler(inputView)
     val promotionValidator = PromotionValidator()
-    val promotionProcessor = PromotionProcessor(
+    val freeItemManager = FreeItemManager()
+    val promotionHandler = PromotionHandler(freeItemManager)
+    val promotionController = PromotionController(
+        inputController = inputController,
         promotionValidator = promotionValidator,
         promotionCalculator = promotionCalculator,
         promotionHandler = promotionHandler,
         freeItemManager = freeItemManager
     )
-    val inputController = InputController(inputView, inputValidator)
-    val receiptManager = ReceiptManager(inputController)
+
+    // Store 및 Product 관련 객체 생성
+    val productParser = ProductParser(promotionParser)
+    val storeManager = StoreManager(productParser, promotionParser)
+
+    // Receipt 관련 객체 생성
+    val receiptManager = ReceiptManager()
+    val receiptController = ReceiptController(receiptManager, freeItemManager, outputView, inputController)
+
+    // Main Controller 생성 및 실행
     val controller = Controller(
         inputController = inputController,
         storeManager = storeManager,
         outputView = outputView,
-        promotionProcessor = promotionProcessor,
-        receiptManager = receiptManager
+        promotionController = promotionController,
+        receiptController = receiptController
     )
 
     controller.run()
