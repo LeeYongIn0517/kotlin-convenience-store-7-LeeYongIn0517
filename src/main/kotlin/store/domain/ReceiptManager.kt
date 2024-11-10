00b5a23609a881c9a4a67995bbf4a77c8641c8cf
이용인 <yongin0517@gmail.com>
2024-11-10 17:32:32 +0900

    refactor: 메서드 순서 변경

--- src/main/kotlin/store/domain/ReceiptManager.kt
+++ src/main/kotlin/store/domain/ReceiptManager.kt
@@ -4,15 +4,16 @@ import store.model.OrderItem
 import store.model.Product
 import store.model.Receipt
 
-class ReceiptManager() {
+class ReceiptManager {
     private var receipt: Receipt = initializeReceipt()
+
+    // 초기화 및 설정 관련 메서드
+    private fun initializeReceipt(): Receipt {
+        return Receipt("W 편의점", listOf(), listOf(), 0, 0, 0, 0, 0)
+    }
 
     fun resetReceipt() {
         receipt = initializeReceipt()
-    }
-
-    private fun initializeReceipt(): Receipt {
-        return Receipt("W 편의점", listOf(), listOf(), 0, 0, 0, 0, 0)
     }
 
     fun setReceiptItems(orderItems: List<OrderItem>) {
@@ -23,6 +24,7 @@ class ReceiptManager() {
         receipt.freeItems = freeItems
     }
 
+    // 계산 관련 메서드
     fun calculateTotalAmount() {
         receipt.totalPrice = receipt.items.sumOf { it.price * it.orderQuantity } +
                 receipt.freeItems.sumOf { it.price * it.orderQuantity }
@@ -45,6 +47,7 @@ class ReceiptManager() {
         receipt.finalAmount = receipt.totalPrice - receipt.promotionDiscount - receipt.membershipDiscount
     }
 
+    // 최종 처리 및 조회 관련 메서드
     fun updateProductStock(products: MutableList<Product>): List<Product> {
         return receipt.updateProductStock(products)
     }
